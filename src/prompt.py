7f51a822b4067f80368f05c6268f6a331b2f54ed
bajajdivya <bajajdivya02@gmail.com>
2024-07-31 19:17:23 +0530

    first commit

---
+++ src/prompt.py
@@ -0,0 +1,13 @@
+
+
+prompt_template = """ 
+
+
+Template:
+Context: {context}
+Question: {question}
+
+Helpful Response:
+[The generated response should be concise and relevant to the provided context and question, focusing on diseases and medicines. If the input is not within the medical domain, the chatbot should indicate its inability to assist on non-medical topics.]
+
+"""
